7d9f286e11cd991006c700eff94719fb062f9a31
henry <henryboy753951@gmail.com>
2023-05-14 00:19:46 +0800

    add Example

--- ExampleToUseCourt.py
+++ ExampleToUseCourt.py
@@ -13,7 +13,12 @@ for id in range(1, 801):
     video_filename = F'val/{id}/{id}.mp4'
     video = cv2.VideoCapture(video_filename)
     success,frame = video.read()
-    a = court.get_court(frame)
-    print(a)
+    a,c = court.get_court(frame)
+    print(c)
+    for p in a:
+        cv2.circle(frame, (int(p[0]), int(p[1])), 5, (0, 0, 255), -1)
+    for p in c:
+        cv2.circle(frame, (int(p[0]), int(p[1])), 5, (0, 255, 255), -1)
+    cv2.imshow('frame',frame)
     cv2.waitKey(0)
     
